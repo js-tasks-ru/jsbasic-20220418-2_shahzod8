a753685d85e14f3fd423b0b6afc09c297a8131f3
Давлатов Шахзод <shahzod@davlatov.space>
2022-04-24 20:26:58 +0300

    Занятие 3: Функции и массивы

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
+const getArrayOfNumbers = (str) => str
+  .split(' ')
+  .map((element) => Number(element))
+  .filter((num) => isFinite(num));
+
 function getMinMax(str) {
-  // ваш код...
+  const arrayNumbers = getArrayOfNumbers(str);
+  const min = Math.min(...arrayNumbers);
+  const max = Math.max(...arrayNumbers);
+
+  return { min, max };
 }
