86d5b106a0fa8733934598c05b19cfa73725773e
Давлатов Шахзод <shahzod@davlatov.space>
2022-05-05 15:17:34 +0300

    Занятие 6: JS классы и компоненты

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,20 +1,25 @@
-export default class ProductCard {
-  constructor(product) {
-    this.elem = this.renderCard(product);
-  }
+const generateCustomEvent = (element, id) => {
+  element.dispatchEvent(new CustomEvent('product-add', {
+    detail: id,
+    bubbles: true,
+  }));
+};
 
-  renderCard(product) {
-    const {
-      name,
-      price,
-      image,
-      id,
-    } = product;
+const renderCardButton = (id) => {
+  const cardButton = document.createElement('button');
+  cardButton.type = 'button';
+  cardButton.classList.add('card__button');
+  cardButton.innerHTML = '<img src="/assets/images/icons/plus-icon.svg" alt="icon">';
+  cardButton.addEventListener('click', ({ target }) => generateCustomEvent(target, id));
 
-    const card = document.createElement('div');
-    card.classList.add('card');
+  return cardButton;
+};
 
-    card.innerHTML = `
+const renderCard = ({ name, price, image, id }) => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
       <div class="card__top">
         <img src="/assets/images/products/${image}" class="card__image" alt="product">
         <span class="card__price">€${price.toFixed(2)}</span>
@@ -24,26 +29,19 @@ export default class ProductCard {
       </div>
     `;
 
-    const cardBody = card.querySelector('.card__body');
-    cardBody.append(this.renderCardButton(id));
+  const cardBody = card.querySelector('.card__body');
+  cardBody.append(renderCardButton(id));
 
-    return card;
+  return card;
+};
+
+export default class ProductCard {
+  constructor(product) {
+    this.product = product;
+    this.elem = this.getElem();
   }
 
-  renderCardButton(id) {
-    const cardButton = document.createElement('button');
-    cardButton.type = 'button';
-    cardButton.classList.add('card__button');
-    cardButton.innerHTML = '<img src="/assets/images/icons/plus-icon.svg" alt="icon">';
-    cardButton.addEventListener('click', ({ target }) => this.generateCustomEvent(target, id));
-
-    return cardButton;
-  }
-
-  generateCustomEvent(element, id) {
-    element.dispatchEvent(new CustomEvent('product-add', {
-      detail: id,
-      bubbles: true,
-    }));
+  getElem() {
+    return renderCard(this.product);
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,39 +1,78 @@
-export default class Carousel {
-  constructor(slides) {
-    this.slides = slides;
-    this.elem = this.renderCarousel();
+const carouselArrows = ['carousel__arrow_right', 'carousel__arrow_left'];
+
+const generateCustomEvent = (element, id) => {
+  element.dispatchEvent(new CustomEvent('product-add', {
+    detail: id,
+    bubbles: true,
+  }));
+};
+
+const mappingArrowsImage = {
+  'carousel__arrow_right': 'angle-icon.svg',
+  'carousel__arrow_left': 'angle-left-icon.svg',
+};
+
+const mappingCarouselArrows = {
+  'carousel__arrow_right': (carouselInner) => {
+    carouselInner.dataset.currentSlide = +carouselInner.dataset.currentSlide + 1;
+  },
+  'carousel__arrow_left': (carouselInner) => {
+    carouselInner.dataset.currentSlide = +carouselInner.dataset.currentSlide - 1;
+  },
+};
+
+const handleDisplayArrow = (number, maxNumber) => {
+  const arrows = document.querySelectorAll('.carousel__arrow');
+
+  if (!arrows) {
+    return;
   }
 
-  renderCarousel() {
-    const carousel = document.createElement('div');
-    carousel.classList.add('carousel');
-    carousel.innerHTML = this.renderCarouselArrow();
-
-    const carouselInner = document.createElement('div');
-    carouselInner.classList.add('carousel__inner');
-
-    this.slides.forEach((slide) => {
-      const carouselSlide = this.renderCarouselSlide(slide);
-      carouselInner.append(carouselSlide);
+  switch (number) {
+  case '0':
+    arrows[1].style.display = 'none';
+    break;
+  case String(maxNumber):
+    arrows[0].style.display = 'none';
+    break;
+  default:
+    arrows.forEach((arrow) => {
+      arrow.style.display = '';
     });
+  }
+};
 
-    carousel.append(carouselInner);
-    return carousel;
+const renderCarouselArrow = (arrow) => {
+  const htmlArrow = document.createElement('div');
+  htmlArrow.classList.add('carousel__arrow', arrow);
+  htmlArrow.innerHTML = `<img src="/assets/images/icons/${mappingArrowsImage[arrow]}" alt="icon">`;
+
+  htmlArrow.addEventListener('click', () => {
+    const carouselInner = document.querySelector('.carousel__inner');
+    const maxSlideNumber = document.querySelectorAll('.carousel__slide').length - 1;
+
+    mappingCarouselArrows[arrow](carouselInner);
+
+    const translateSize = -carouselInner.dataset.currentSlide * carouselInner.offsetWidth;
+
+    carouselInner.style.transform = `translateX(${translateSize}px)`;
+
+    handleDisplayArrow(carouselInner.dataset.currentSlide, maxSlideNumber);
+  });
+
+  if (arrow === 'carousel__arrow_left') {
+    htmlArrow.style.display = 'none';
   }
 
-  renderCarouselSlide(slide) {
-    const {
-      name,
-      price,
-      image,
-      id
-    } = slide;
+  return htmlArrow;
+};
 
-    const carouselSlide = document.createElement('div');
-    carouselSlide.classList.add('carousel__slide');
-    carouselSlide.dataset.id = id;
+const renderCarouselSlide = ({ name, price, image, id }) => {
+  const carouselSlide = document.createElement('div');
+  carouselSlide.classList.add('carousel__slide');
+  carouselSlide.dataset.id = id;
 
-    carouselSlide.innerHTML = `
+  carouselSlide.innerHTML = `
       <img src="/assets/images/carousel/${image}" class="carousel__img" alt="slide">
         <div class="carousel__caption">
           <span class="carousel__price">€${price.toFixed(2)}</span>
@@ -44,27 +83,40 @@ export default class Carousel {
         </div>
     `;
 
-    const carouselButton = carouselSlide.querySelector('button');
-    carouselButton.addEventListener('click', ({ target }) => this.generateCustomEvent(target, id));
+  const carouselButton = carouselSlide.querySelector('button');
+  carouselButton.addEventListener('click', ({ target }) => generateCustomEvent(target, id));
 
-    return carouselSlide;
+  return carouselSlide;
+};
+
+const renderCarousel = (slides) => {
+  const carousel = document.createElement('div');
+  carousel.classList.add('carousel');
+
+  carouselArrows.forEach((arrow) => {
+    carousel.append(renderCarouselArrow(arrow));
+  });
+
+  const carouselInner = document.createElement('div');
+  carouselInner.classList.add('carousel__inner');
+  carouselInner.dataset.currentSlide = '0';
+
+  slides.forEach((slide) => {
+    const carouselSlide = renderCarouselSlide(slide);
+    carouselInner.append(carouselSlide);
+  });
+
+  carousel.append(carouselInner);
+  return carousel;
+};
+
+export default class Carousel {
+  constructor(slides) {
+    this.slides = slides;
+    this.elem = this.getElem();
   }
 
-  renderCarouselArrow() {
-    return `
-      <div class="carousel__arrow carousel__arrow_right">
-        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
-      </div>
-      <div class="carousel__arrow carousel__arrow_left">
-        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
-      </div>
-    `;
-  }
-
-  generateCustomEvent(element, id) {
-    element.dispatchEvent(new CustomEvent('product-add', {
-      detail: id,
-      bubbles: true,
-    }));
+  getElem() {
+    return renderCarousel(this.slides);
   }
 }
