6d25a19cb9225390603a22069c8bbb9ae2b77dd3
Давлатов Шахзод <shahzod@davlatov.space>
2022-05-12 19:56:25 +0300

    Отредактирован код карточки товара

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,47 +1,44 @@
-const generateCustomEvent = (element, id) => {
-  element.dispatchEvent(new CustomEvent('product-add', {
-    detail: id,
-    bubbles: true,
-  }));
-};
-
-const renderCardButton = (id) => {
-  const cardButton = document.createElement('button');
-  cardButton.type = 'button';
-  cardButton.classList.add('card__button');
-  cardButton.innerHTML = '<img src="/assets/images/icons/plus-icon.svg" alt="icon">';
-  cardButton.addEventListener('click', ({ target }) => generateCustomEvent(target, id));
-
-  return cardButton;
-};
-
 const renderCard = ({ name, price, image, id }) => {
   const card = document.createElement('div');
   card.classList.add('card');
 
   card.innerHTML = `
-      <div class="card__top">
-        <img src="/assets/images/products/${image}" class="card__image" alt="product">
-        <span class="card__price">€${price.toFixed(2)}</span>
-      </div>
-      <div class="card__body">
-        <div class="card__title">${name}</div>
-      </div>
-    `;
-
-  const cardBody = card.querySelector('.card__body');
-  cardBody.append(renderCardButton(id));
+    <div class="card__top">
+      <img src="/assets/images/products/${image}" class="card__image" alt="product">
+      <span class="card__price">€${price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+      <div class="card__title">${name}</div>
+      <button type="button" class="card__button" data-id="${id}">
+        <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+      </button>
+    </div>
+  `;
 
   return card;
 };
 
+const addEventListener = (container) => {
+  const cardButton = container.querySelector('.card__button');
+
+  cardButton.addEventListener('click', () => {
+    cardButton.dispatchEvent(new CustomEvent('product-add', {
+      detail: cardButton.dataset.id,
+      bubbles: true,
+    }));
+  });
+};
+
 export default class ProductCard {
+  #product;
+
   constructor(product) {
-    this.product = product;
-    this.elem = this.getElem();
+    this.#product = product;
+    this.#render();
   }
 
-  getElem() {
-    return renderCard(this.product);
+  #render() {
+    this.elem = renderCard(this.#product);
+    addEventListener(this.elem);
   }
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -61,4 +61,3 @@ export default class StepSlider extends OldStepSlider {
     addDragAndDrop(this.elem, this._steps);
   }
 }
-
